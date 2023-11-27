bd5155b7374f86375c26156d92ef2717ea15bfba
AbbosxonDeveloper <abbosxonoripov3@gmail.com>
2023-11-27 15:05:52 +0500

    initial

---
+++ script.js
@@ -0,0 +1,20 @@
+// const display = document.querySelector('.display');
+// const buttons = document.querySelectorAll('button');
+
+// buttons.forEach(button => {
+//     button.addEventListener('click', () => {
+//         if (button.textContent === 'C') {
+//             clearDisplay();
+//         } else if (button.textContent === '&#8592;') {
+//             deleteLast();
+//         }
+//     })
+// })
+
+// function clearDisplay(){
+//     display.textContent = "";
+// }
+
+// function addToDisplay(sign){
+//     display.
+// }
